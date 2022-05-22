aff6c4b38b9737618fc7dae58d7f52018b66ae78
jeremie <mandin.j@gmail.com>
2022-05-22 20:29:21 +0200

    swiper / carousel

--- src/container/Home/index.jsx
+++ src/container/Home/index.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import SwiperFade from '../../components/SwiperFade';
 
 import './home.scss'
 const Home = () => {
   return (
     <div className='containerHome'>
-      <h1>Home</h1>
+      <SwiperFade />
     </div>
   );
 }
